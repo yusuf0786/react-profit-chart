daddcde35e4af68c00ae54b38ba0d38f04248615
yusuf <yusssuf0786@gmail.com>
2023-12-24 02:40:10 +0530

    twiked the vite.config.ts file to resolve the error

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  esbuild: {
+    jsxFactory: 'React.createElement',
+    jsxFragment: 'React.Fragment',
+    target: 'ES2021', // Set the target environment explicitly
+  },
 })
